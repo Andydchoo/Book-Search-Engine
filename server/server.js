5b12da1487bc33c596787e24a6ede9d7fae1470d
Andrew Choo <andydchoo@gmail.com>
2021-11-15 18:14:04 -0500

    s

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const server = new ApolloServer({
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
